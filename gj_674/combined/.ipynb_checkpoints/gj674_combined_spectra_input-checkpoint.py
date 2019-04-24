da308684fe78ca595c4f88ce0cf42799ca767725
davidjwilson <djwilson394@gmail.com>
2019-04-24 11:05:40 -0600

    combined_script_checkpoint

--- gj_674/combined/.ipynb_checkpoints/gj674_combined_spectra_input-checkpoint.py
+++ gj_674/combined/.ipynb_checkpoints/gj674_combined_spectra_input-checkpoint.py
@@ -24,17 +24,54 @@ filepaths = {'xmm':'xmm/GJ674.fits',
              'lya':'/lya/GJ674_intrinsic_LyA_profile.txt',
              'stis_g140m':'/STIS/GJ674_G140M_coadd.ecsv',
              'stis_g140l':'/STIS/GJ674_G140L_noflare_x1d.fits',
-             'stis_g230L':'/STIS/GJ674_G230L_x1d.fits',
-             'stis_g430L':'/STIS/odlm21010_sx1.fits',
+             'stis_g230l':'/STIS/GJ674_G230L_x1d.fits',
+             'stis_g430l':'/STIS/odlm21010_sx1.fits',
              'phoenix':'/photometry/scaled_03400-4.50-0.0_phoenix_gj674.ecsv'}
 
-cosdata = sc.read_idl(path+filepaths['cos_g130m'])
-w = cosdata['w']
-glowmask = (w <1207)|(w >1225)&(w <1304)|(w >1304.5)&(w <1355)|(w >1356)   #mask out lya, airglow
-w, f, cos_additions = sc.masked_additions(cosdata, glowmask)
-sc.plot_spectrum(w,f)
-totals = sc.add_spec(totals, cos_additions)
+#COS
+#G130M
+g130m_data = sc.read_idl(path+filepaths['cos_g130m'])
+w = g130m_data['w']
+lya_edges = [1207, 1225] #lyman alpha region to remove
+airglow = [1304, 1304.5, 1355, 1356] #oi airglow to remove
+glowmask = (w <lya_edges[0])|(w > lya_edges[1])&(w <airglow[0])|(w >airglow[1])&(w <airglow[2])|(w >airglow[3])   #mask out lya, airglow
+g130m_start, g130m_end, totals = sc.make_section(totals, g130m_data, mask =glowmask)
 
+#lya 
+g140m_normfac = 2.9 #to do - calculate scales in spectra_combine
+lya_data = sc.read_lyamod(path+filepaths['lya'])
+lya_start, lya_end,totals = sc.make_section(totals, lya_data, normfac=g140m_normfac)
+
+#G140M
+g140m_data = sc.read_ecsv(path+filepaths['stis_g140m'])
+w = g140m_data['w']
+mask = (w >lya_edges[0])&(w <lya_start)|(w >lya_end)&(w < lya_edges[1]) #include just bits not co
+g140m_start, g140m_end, totals = sc.make_section(totals, g140m_data, mask =mask, normfac=g140m_normfac)
+
+#G140L
+#includes all data blue-wards of the g130m and fills in the airglow gaps
+g140l_normfac = 3.5
+g140l_data = sc.read_stis_x1d(path+filepaths['stis_g140l'])
+w = g140l_data['w']
+mask = (w > airglow[0])&(w < airglow[1])|(w > airglow[2])&(w < airglow[3])|(w >g130m_end) #only need the bit not covered by cos, plus airglow filler
+g140l_start, g140l_end, totals = sc.make_section(totals, g140l_data, mask =mask, normfac=g140l_normfac)
+
+#G230L
+g230l_normfac = 1.43
+g230l_data = sc.read_stis_x1d(path+filepaths['stis_g230l'])
+w = g230l_data['w']
+g230l_clip = [0,-6]
+mask = (w > g140l_end)
+g230l_start, g230l_end, totals = sc.make_section(totals, g230l_data, mask =mask, normfac=g230l_normfac, clip = g230l_clip)
+
+#G430L 
+# normalised to PHOENIX
+g430l_normfac = 1.11
+g430l_data = sc.read_stis_ccd(path+filepaths['stis_g430l'])
+w = g430l_data['w']
+g430l_clip = [0,-1]
+mask = (w > g230l_end)
+g430l_start, g430l_end, totals = sc.make_section(totals, g430l_data, mask =mask, normfac=g430l_normfac, clip = g430l_clip)
 
 
 
@@ -46,5 +83,6 @@ plt.ylabel('Flux (erg s$^{-1}$cm$^{-2}$\AA$^{-1}$)', size=20)
 plt.axhline(0, ls='--', c='k')
 
 totals = sc.sort_totals(totals)
+#print(totals)
 
 plt.show()
