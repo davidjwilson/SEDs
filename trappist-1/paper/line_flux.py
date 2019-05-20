c0f29692d7624e58f170559cb0af6212c2e0c7ee
davidjwilson <djwilson394@gmail.com>
2019-05-20 16:30:11 -0500

    fluxes v phoenix

--- trappist-1/paper/line_flux.py
+++ trappist-1/paper/line_flux.py
@@ -70,7 +70,7 @@ mgii_2800 = [2795.528]
 
 all_lines = [ciii_1175, feii_1200, siiii_1206, [nv_1240[0]], [nv_1240[1]], ci_1288, [cii_1335[0]],[cii_1335[1]], feii_1527, [civ_1550[0]],[civ_1550[1]], mgii_2800]
 
-line_edges = [[1174.6,1176.1],[1198.4,1200.95],[1205.9,1206.8],[1238.2,1239.9],[1242.3,1242.75],[1288.5,1288.8],[1333.9, 1334.55],[1334.9,1335.9],[1526.8, 1527.2],[1547.65, 1548.3],[1550.25,1550.75],[2794.,2797]]
+line_edges = [[1174.6,1176.1],[1198.4,1200.95],[1205.9,1206.8],[1238.2,1238.9],[1242.3,1242.75],[1288.5,1288.8],[1333.9, 1334.55],[1334.9,1335.9],[1526.8, 1527.2],[1547.65, 1548.3],[1550.25,1550.75],[2794.,2797]]
 b_bg = [[1173., 1174.],[1197., 1198.],[1205.5, 1205.75],[1237.5, 1238.],[1241.5, 1242.], [1287.5, 1288.],[1332., 1333.],[1332., 1333.],[1525., 1526.],[1546.,1547.],[1549.,1550],[2791,2793.]]
 
 r_bg = [[1176.25, 1176.5],[1201., 1203.],[1204., 1205.],[1239.5, 1240.],[1243., 1244.],[1289., 1290.],[1334.6,1334.8],[1334.6,1334.8],[1527.5,1528.5],[1548.5,1549.],[1551,1552],[2798., 2799.]]
@@ -97,6 +97,8 @@ dshift = (1.0+(dv/c))
 
 gauss_int  = []
 gauss_e = []
+#line_b = []
+#line_r = []
 
 fitter = fitting.SLSQPLSQFitter()
 
@@ -138,8 +140,8 @@ for i in range(len(plotlines)):
     
     l_flux = np.trapz(gg_fit(w1)[(w1 > line_edges[i][0]) & (w1 < line_edges[i][1])], w1[(w1 > line_edges[i][0]) & (w1 < line_edges[i][1])])
     gauss_int.append(l_flux)
-    b_bg_flux = np.trapz(f1[(w1 > b_bg[i][0]) & (w1 < b_bg[i][1])], w1[(w1 > b_bg[i][0]) & (w1 < b_bg[i][1])])*b_scale[i]
-    r_bg_flux = np.trapz(f1[(w1 > r_bg[i][0]) & (w1 < r_bg[i][1])], w1[(w1 > r_bg[i][0]) & (w1 < r_bg[i][1])])*r_scale[i]
+    b_bg_flux = np.trapz(f1[(w1 > b_bg[i][0]) & (w1 < b_bg[i][1])], w1[(w1 > b_bg[i][0]) & (w1 < b_bg[i][1])])*b_scale[i][0]
+    r_bg_flux = np.trapz(f1[(w1 > r_bg[i][0]) & (w1 < r_bg[i][1])], w1[(w1 > r_bg[i][0]) & (w1 < r_bg[i][1])])*r_scale[i][0]
     gauss_e.append((abs(b_bg_flux)+abs(r_bg_flux))/2.)
     
     #print(plot_name[i],lines[i],l_flux,(abs(b_bg_flux)+abs(r_bg_flux))/2. )
@@ -152,5 +154,9 @@ for i in range(len(plotlines)):
 for a,b, c, d in zip(plot_name,lines,gauss_int,gauss_e):
     print(a,b,c,d)
 
+#print(gauss_e)
+savdat = Table([plot_name, lines, gauss_int, gauss_e, np.array(line_edges)[:,0], np.array(line_edges)[:,1]], names = ['Species', 'lambda', 'int_flux', 'int_error', 'blue_edge', 'red_edge'])
+ascii.write(savdat, 'int_flux_table.ecsv', format='ecsv', overwrite=True)
+    
 plt.show()
         
