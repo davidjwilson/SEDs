9aafedb65f7b9122cc0efec91e740752c77bcdc6
davidjwilson <djwilson394@gmail.com>
2023-02-10 16:49:43 -0700

    fixed bad g140m files

--- lya/get_center.py
+++ lya/get_center.py
@@ -25,30 +25,31 @@ def on_key(event):
         plt.close()
     
 #path = '/home/david/work/muscles/trappist-1/hst/g140m_cals/all_obs/'
-path = 'data/'
+# path = 'data/'
+path = '/media/david/2tb_ext_hd/hddata/mega_muscles/data/g140m/'
 flts = glob.glob(path +'*flt.fits')
 
 roots = []
 #only the stars that need new lya extractions
-stars = ['GJ1132', 'GJ163', 'L-980-5']
+# stars = ['GJ1132', 'GJ163', 'LHS2686']
 #o_roots = ['od3v02010', 'od3v03010', 'od3v01020', 'od3v01010']
 
 for flt in flts:
     target = fits.getheader(flt,0)['TARGNAME']
     rootname = fits.getheader(flt,0)['ROOTNAME']
-    if target in stars:
-        roots.append(rootname)
-        data = fits.getdata(flt,1)
-        fig = plt.figure(target+'_'+rootname, figsize=(13,13))
-        plt.imshow(data, cmap=cm.gray_r, norm = LogNorm())
-        plt.tight_layout()
-        #plt.axhline(500, c='r', ls ='--')
-        #plt.xlim(300, 600)
-        #plt.ylim(350,650)
-        #plt.grid()
-        cid = fig.canvas.mpl_connect('key_press_event',on_key)
-        plt.show()
-        #plt.close()
+    # if target in stars:
+    roots.append(rootname)
+    data = fits.getdata(flt,1)
+    fig = plt.figure(target+'_'+rootname, figsize=(13,13))
+    plt.imshow(data, cmap=cm.gray_r, norm = LogNorm())
+    plt.tight_layout()
+    #plt.axhline(500, c='r', ls ='--')
+    #plt.xlim(300, 600)
+    #plt.ylim(350,650)
+    #plt.grid()
+    cid = fig.canvas.mpl_connect('key_press_event',on_key)
+    plt.show()
+    #plt.close()
 
     
 savedat = Table([roots, x1, y1, y2], names=['ROOTNAME', 'XCOL', 'A2CENTER', 'UNCERTAIN'])
